3947d0664ce9d2eb582910b36311a3008b3fe877
simonbeirouti <hello@simonbeirouti.com>
2022-07-06 13:41:26 +1000

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: ["./pages/**/*.js", "./components/**/*.js"],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
