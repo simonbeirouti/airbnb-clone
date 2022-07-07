f2a9f84b5a59837dc0d2ac72dff3b6982f62f5b7
simonbeirouti <hello@simonbeirouti.com>
2022-07-07 23:37:00 +1000

    load react-map-gl and key to mapbox

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoiYmVzaW1vbiIsImEiOiJjbDVhdGU0ZXEwMTA2M3FwZ3VyZHJsZTR4In0.QM638duCeWA-pXPvf2ChSQ",
+  },
 };
 
 module.exports = nextConfig;
