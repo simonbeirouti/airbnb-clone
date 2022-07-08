7b138355b7757e6832482880e45caed7b3d7afbc
simonbeirouti <hello@simonbeirouti.com>
2022-07-08 20:19:02 +1000

    add geolib & center mapbox with pointers and popup

--- components/MapBox.js
+++ components/MapBox.js
@@ -1,21 +1,55 @@
 import { useState } from "react";
-import Map from "react-map-gl";
+import ReactMapGl, { Marker, Popup } from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { getCenter } from "geolib";
 
-export default function MapBox() {
+export default function MapBox({ searchResults }) {
+  const [selectedLocation, setSelectedLocation] = useState({});
+
+  const coordinates = searchResults.map((result) => ({
+    longitude: result.long,
+    latitude: result.lat,
+  }));
+
+  const center = getCenter(coordinates);
+
   const [viewport, setViewPort] = useState({
     width: "100%",
     height: "100%",
-    latitude: 37.7577,
-    longitude: -122.4376,
+    latitude: center.latitude,
+    longitude: center.longitude,
     zoom: 11,
   });
+
   return (
-    <Map
+    <ReactMapGl
       mapStyle="mapbox://styles/besimon/cl5au7mdm008p17p881tss7f8"
       mapboxAccessToken={process.env.mapbox_key}
       {...viewport}
       onViewportChange={(viewport) => setViewPort(viewport)}
-    ></Map>
+    >
+      {searchResults.map((result) => (
+        <div key={result.long}>
+          <Marker
+            longitude={result.long}
+            latitude={result.lat}
+            offsetLeft={-20}
+            offsetTop={-10}
+          />
+          {selectedLocation.long === result.long ? (
+            <Popup
+              onClose={() => setSelectedLocation({})}
+              closeOnClick={true}
+              latitude={result.lat}
+              longitude={result.long}
+            >
+              {result.title}
+            </Popup>
+          ) : (
+            false
+          )}
+        </div>
+      ))}
+    </ReactMapGl>
   );
 }
